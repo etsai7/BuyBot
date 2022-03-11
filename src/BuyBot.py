3d888c28815b995e87aa36ec1de425e580ff8c09
Eric Tsai <e.tsai70@gmail.com>
2022-03-11 16:54:03 -0500

    Accessing page of items and pulling out info of each item. Used pandas to place in table.

--- src/BuyBot.py
+++ src/BuyBot.py
@@ -1,13 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import pandas as pd
 
+# 1. Open up website: NewEgg directly to gpu link - prob w/ filters intact
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-# driver.close()
+driver.get("https://www.newegg.com/p/pl?N=100007709%20601385735%20601357248%20601357247")
+
+# 2. Grab all available gpus using Beautiful Soup
+soup = BeautifulSoup(driver.page_source, features="html.parser")
+
+# Grab the container containing all listed items
+items = soup.find("div", {"class": "item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell"})
+
+# Go through each of the items in the list
+itemsProcessed = []
+for item in items.findAll("div"):
+    itemData = []
+    itemTitle = item.find("a", {"class": "item-title"})
+    itemAvailability = item.find("p", {"class": "item-promo"})
+
+    status = "Available"
+
+    if itemAvailability and itemAvailability == "OUT OF STOCK":
+        status = "Sold Out"
+
+    if itemTitle:
+        itemData.append(itemTitle.text)
+        itemData.append(status)
+
+    if itemData:
+        itemsProcessed.append(itemData)
+
+df = pd.DataFrame.from_records(itemsProcessed, columns = ["Item Title", "Status"])
+print(df)
+
+# assert "Python" in driver.title
+# elem = driver.find_element(By.NAME, "q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+driver.close()
+
+# 1. Open up website: NewEgg directly to gpu link - prob w/ filters intact
+# 2. Grab all available gpus using Beautiful Soup
+# 3. Add filters
+#     - Only ones in stock
+#     - Sort by pricing
+#     - Maybe in brand or type
+
+# May need to have a bunch of try catches
+
+# URL: https://www.newegg.com/p/pl?N=100007709%20601385735%20601357248%20601357247
+# Container for all listed items: /html/body/div[7]/div[4]/section/div/div/div[2]/div/div/div/div[2]/div[1]/div[2]/div[2]
+# A single item cell: //*[@id="item_cell_14-932-327_1_0"]
